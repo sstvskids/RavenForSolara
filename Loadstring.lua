bd2e11c56a05972281c7ff7b627c36097f1e8836
Near <fynncooolooo@gmail.com>
2024-11-02 23:46:16 +0100

    Create Loadstring.lua

---
+++ Loadstring.lua
@@ -0,0 +1,3 @@
+--still not out for the public, please wait for an annoucement on v3rm.net!
+--thanks in advance!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Near-B4/Raven-B4-For-Roblox/refs/heads/main/Raven%20B4%20Loader.lua"))
