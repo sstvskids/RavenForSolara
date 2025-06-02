76bb92743f7d84dd91e734aeb5ad36f5047f7c65
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-06-02 19:21:57 +1000

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,3 +1,1 @@
---still not out for the public, please wait for an annoucement on v3rm.net!
---thanks in advance!
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Near-B4/Raven-B4-For-Roblox/refs/heads/main/Raven%20B4%20Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/RavenForSolara/refs/heads/main/Raven%20B4%20Loader.lua"))()
